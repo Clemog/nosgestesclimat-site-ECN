637ffdb0d67ae8fa05a222b5c00185af00a9a184
Clement AUGER <clement.auger@eleves.ec-nantes.fr>
2021-01-13 11:58:08 +0100

    Ajout faunadb et changement variable env

--- functions/postanswer.js
+++ functions/postanswer.js
@@ -2,7 +2,7 @@
 const faunadb = require("faunadb")
 
 const q = faunadb.query
-const faunaClient = new faunadb.Client({ secret: process.env.FAUNADB_SERVER_SECRET })
+const faunaClient = new faunadb.Client({ secret: process.env.FAUNADB_ADMIN_SECRET })
 
 exports.handler = async (event, context) => {
   let resp
