fdb7f6c046720dc6cbafd582c78a51b41fb6ace9
Clement AUGER <clement.auger@eleves.ec-nantes.fr>
2021-01-13 14:34:56 +0100

    Update AddAnswer.js

--- source/sites/publicodes/AddAnswer.js
+++ source/sites/publicodes/AddAnswer.js
@@ -1,9 +1,17 @@
 import React, { useState } from 'react'
-
-
-
-
-
+import { useLocation, useParams } from 'react-router'
+import emoji from 'react-easy-emoji'
+import tinygradient from 'tinygradient'
+import { animated, useSpring } from 'react-spring'
+import ShareButton from 'Components/ShareButton'
+import { findContrastedTextColor } from 'Components/utils/colors'
+import { motion } from 'framer-motion'
+import BallonGES from './images/ballonGES.svg'
+import StartingBlock from './images/starting block.svg'
+import SessionBar from 'Components/SessionBar'
+import AddAnswer from './AddAnswer'
+import Chart from './chart'
+import { Link } from 'react-router-dom'
 
 
 const [thought, setThought] = useState({ date: new Date().toISOString().split('T')[0], text: 'Thisis a test' });
@@ -17,4 +25,5 @@ const AddAnswer = async () => {
 	const { error, message } = await resp.json()
 	error ? console.error(error) : console.log(message)
 }
+
 export default AddAnswer
