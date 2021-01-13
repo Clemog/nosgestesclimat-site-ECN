12352a5c1e0be0d426c35d19d3ae57fdca402e63
Clement AUGER <clement.auger@eleves.ec-nantes.fr>
2021-01-13 15:42:45 +0100

    test avec value

--- source/sites/publicodes/AddAnswer.js
+++ source/sites/publicodes/AddAnswer.js
@@ -1,12 +1,11 @@
 import React, { useState } from 'react'
-
-
+import Value from 'Components/Value'
 /*const [thought, setThought] = useState({ date: new Date().toISOString().split('T')[0], text: 'Thisis a test' });*/
 
 const AddAnswer = async () => {
   	const resp = await fetch('/.netlify/functions/postanswer', { 
   		method: 'POST',
-  		body: JSON.stringify({ date: new Date().toISOString().split('T')[0], text: 'Thisis a test' })
+  		body: JSON.stringify(Value})
   	})
     
 	const { error, message } = await resp.json()
