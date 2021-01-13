d000f46574f50b2607a42cd8fa019cbd854fd866
Clement AUGER <clement.auger@eleves.ec-nantes.fr>
2021-01-13 15:45:19 +0100

    Revert "test avec value"
    This reverts commit 12352a5c1e0be0d426c35d19d3ae57fdca402e63.

--- source/sites/publicodes/AddAnswer.js
+++ source/sites/publicodes/AddAnswer.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react'
-import Value from 'Components/Value'
+
+
 /*const [thought, setThought] = useState({ date: new Date().toISOString().split('T')[0], text: 'Thisis a test' });*/
 
 const AddAnswer = async () => {
   	const resp = await fetch('/.netlify/functions/postanswer', { 
   		method: 'POST',
-  		body: JSON.stringify(Value})
+  		body: JSON.stringify({ date: new Date().toISOString().split('T')[0], text: 'Thisis a test' })
   	})
     
 	const { error, message } = await resp.json()
