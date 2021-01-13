3a926a170d8b5b3b1c4b3d36d21861a284cc6803
Clement AUGER <clement.auger@eleves.ec-nantes.fr>
2021-01-13 15:09:26 +0100

    Update AddAnswer.js

--- source/sites/publicodes/AddAnswer.js
+++ source/sites/publicodes/AddAnswer.js
@@ -1,23 +1,12 @@
 import React, { useState } from 'react'
-import emoji from 'react-easy-emoji'
-import tinygradient from 'tinygradient'
-import { animated, useSpring } from 'react-spring'
-import ShareButton from 'Components/ShareButton'
-import { findContrastedTextColor } from 'Components/utils/colors'
-import { motion } from 'framer-motion'
-import BallonGES from './images/ballonGES.svg'
-import StartingBlock from './images/starting block.svg'
-import SessionBar from 'Components/SessionBar'
-import Chart from './chart'
-import { Link } from 'react-router-dom'
 
 
-const [thought, setThought] = useState({ date: new Date().toISOString().split('T')[0], text: 'Thisis a test' });
+/*const [thought, setThought] = useState({ date: new Date().toISOString().split('T')[0], text: 'Thisis a test' });*/
 
 const AddAnswer = async () => {
   	const resp = await fetch('/.netlify/functions/postanswer', { 
   		method: 'POST',
-  		body: JSON.stringify(thought)
+  		body: JSON.stringify({ date: new Date().toISOString().split('T')[0], text: 'Thisis a test' })
   	})
     
 	const { error, message } = await resp.json()
