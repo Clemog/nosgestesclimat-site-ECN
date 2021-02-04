45eebfab012f307df6b115f6a5564f22bfb14afc
Clement AUGER <clement.auger@eleves.ec-nantes.fr>
2021-02-04 12:11:20 +0100

    Update postanswer.js

--- functions/postanswer.js
+++ functions/postanswer.js
@@ -2,20 +2,20 @@
 const faunadb = require("faunadb")
 
 const q = faunadb.query
-const faunaClient = new faunadb.Client({ secret: process.env.FAUNADB_ADMIN_SECRET })
+const faunaClient = new faunadb.Client({ secret: process.env.FAUNADB_SERVER_SECRET })
 
 exports.handler = async (event, context) => {
-  let resp
+	let resp
 
 	try {
-  		const answer = {
-    		data: JSON.parse(event.body)
-  		}
-  		const req = await faunaClient.query(q.Create(q.Ref("classes/answers"), answer))
-  		console.log(req)
-  		return { statusCode: 200, body: JSON.stringify({ message: 'Successfully added answer!' }) }
+		const answer = {
+			data: JSON.parse(event.body)
+		}
+		const req = await faunaClient.query(q.Create(q.Ref("classes/answers"), answer))
+		console.log(req)
+		return { statusCode: 200, body: JSON.stringify({ message: 'Successfully added answer!' }) }
 	} catch (err) {
-  		return { statusCode: 500, body: JSON.stringify({ error: err.message}) }
+		return { statusCode: 500, body: JSON.stringify({ error: err.message }) }
 	}
 
 }
